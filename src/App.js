dbaaaa6b65f324dd18f79e8f8196a7cb36ebf2e2
Hyungchul You <teoyou881@gmail.com>
2023-07-25 17:10:37 -0400

    Handling SPA initial loading

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import Search from "./search/container/Search";
 import "antd/dist/antd.css";
 import { Route } from "react-router-dom/cjs/react-router-dom";
 import User from "./user/container/User";
+import { useEffect } from "react";
 
 export default function App() {
+    //앱 마운트되는 시점에 loadingEl을 지워주자.
+    useEffect(() => {
+        const bodyEl = document.getElementsByTagName("body")[0];
+        const loadingEl = document.getElementById("init-loading");
+        bodyEl.removeChild(loadingEl);
+    }, []);
+
     return (
         <>
             <Route exact path="/" component={Search}></Route>
